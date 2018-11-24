78da668679b42921a7f3536f338fb497c2c735f4
GavinB90 <gbrooker90@gmail.com>
2018-11-24 12:33:18 +0000

    Day one challenges completed.

---
+++ AdventOfCode2017/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+
+namespace AdventOfCode2017
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Day1Part1 day1Part1 = new Day1Part1();
+            Day1Part2 day1Part2 = new Day1Part2();
+        }
+    }
+}
